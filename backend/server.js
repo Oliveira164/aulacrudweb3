0e99a9f6c8bb5c99e85e9eaf20090a62b6bd2b23
Oliveira <gustavooliveiracosta349@gmail.com>
2026-05-28 20:31:54 +0000

    update

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,64 @@ conexao.connect((erro) =>{
 })
 
 app.post("/alunos", (req, res)=>{
-    const {nome, emaail, curso}= req.body
+    const {nome, email, curso} = req.body
     const sql = `INSERT INTO alunos(nome,email,curso) VALUES (?,?,?)`
 
     conexao.query(
         sql,
-        [nome, emaail, curso],
+        [nome, email, curso],
+        (erro, resultado)=>{
+            if(erro){
+                console.log(erro)
+                res.status(500).json(erro)
+            } else {
+                res.json(resultado)
+            }
+        }
+    )
+})
+
+app.get("/alunos", (req, res)=>{
+    conexao.query(
+        "SELECT * FROM alunos",
+        (erro, resultado)=> {
+            if(erro){
+                console.log('Erro: ', erro)
+                res.status(500).json(erro)
+            } else {
+                res.json(resultado)
+            }
+        }
+    )
+})
+
+app.put("/alunos/:id", (req, res)=> {
+    const {id} = req.params
+    const {nome, email, curso} = req.body
+
+    const sql = `UPDATE alunos SET nome=?, email=?, curso=? WHERE id=?`
+
+    conexao.query(
+        sql,
+        [nome, email, curso, id],
+        (erro, resultado)=>{
+            if(erro){
+                console.log(erro)
+                res.status(500).json(erro)
+            } else {
+                res.json(resultado)
+            }
+        }
+    )
+})
+
+app.delete("/alunos/:id", (req, res)=>{
+    const {id} = req.params
+    const sql = "DELETE FROM alunos WHERE id =?"
+
+    conexao.query(
+        sql,
+        [id],
         (erro, resultado)=>{
             if(erro){
                 console.log(erro)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,21 +11,61 @@ function App(){
   const [editando, setEditando] = useState(false)
   const [idAtual, setIdAtual] = useState(null)
 
+  const API_URL = "http://localhost:3001"
+
+  async function buscarAlunos(){
+    try {
+      const resposta = await axios.get(`${API_URL}/alunos`)
+      setAlunos(resposta.data)
+    } catch(erro) {
+      console.log('Erro ao buscar alunos: ', erro)
+    }
+  }
+
+  useEffect(()=>{buscarAlunos();},[])
 
   async function salvar(e){
     e.preventDefault()
-    const aluno = [nome, email, curso]
+    const aluno = {nome, email, curso}
 
-    if(editando){
-      await axios.put(`https://orange-space-guide-v6wvpxrjq9p6cxg99-3001.app.github.dev/${idAtual}`, aluno)
-      setEditando(false)
-      setIdAtual(null)
-    } else {
-      await axios.post("https://orange-space-guide-v6wvpxrjq9p6cxg99-3001.app.github.dev/", aluno)
-      //limparFormulario()
-      //buscarAlunos()
+    try {
+      if(editando){
+        await axios.put(`${API_URL}/alunos/${idAtual}`, aluno)
+        setEditando(false)
+        setIdAtual(null)
+      } else {
+        await axios.post(`${API_URL}/alunos`, aluno)
+      }
+      limparFormulario()
+      buscarAlunos()
+    } catch(erro) {
+      console.log('Erro ao salvar:', erro)
     }
   }
+
+  async function excluir(id){
+    try {
+      await axios.delete(`${API_URL}/alunos/${id}`)
+      buscarAlunos()
+    } catch(erro) {
+      console.log('Erro ao excluir:', erro)
+    }
+  }
+
+  function limparFormulario(){
+    setNome('')
+    setEmail('')
+    setCurso('')
+  }
+
+  function editar(aluno){
+    setNome(aluno.nome)
+    setEmail(aluno.email)
+    setCurso(aluno.curso)
+    setIdAtual(aluno.id)
+    setEditando(true)
+  }
+
   return(
     <div style={{padding:20}}>
       <h1>CRUD de Alunos</h1>
@@ -55,6 +95,21 @@ function App(){
           {editando ? "Atualizar" : "Cadastrar"}
         </button>
       </form>
+
+      <hr />
+
+      {
+        alunos.map((aluno)=>(
+        <div key={aluno.id}>
+          <h3>{aluno.nome}</h3>
+          <p>{aluno.email}</p>
+          <p>{aluno.curso}</p>
+          <button onClick={()=> editar(aluno)}>Editar</button>
+          <button onClick={()=> excluir(aluno.id)}>Excluir</button>
+          <hr />
+        </div>
+        ))
+      }
     </div>
   )
 }
